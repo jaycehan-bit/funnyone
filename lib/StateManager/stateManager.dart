b13187dd1928dcac716ff17cd357d6c74574cb95
jaycehan-bit <1019010780@qq.com>
2023-09-23 19:00:20 +0800

    混合管理状态

--- lib/StateManager/stateManager.dart
+++ lib/StateManager/stateManager.dart
@@ -38,8 +38,10 @@ class _TapStateA extends State<TapBoxA> {
   }
 }
 
+// 父widget管理状态
+
 class TapBoxB extends StatelessWidget {
-  TapBoxB({Key? key, this.active: false, required this.onChanged})
+  TapBoxB({Key? key, this.active = false, required this.onChanged})
       : super(key: key);
 
   final bool active;
@@ -91,4 +93,99 @@ class _ParsentWidgetState extends State<ParentWidget> {
       ),
     );
   }
+}
+
+// 混合管理状态
+
+class ExternalWidget extends StatefulWidget {
+  @override
+  ExternalWidgetState createState() => ExternalWidgetState();
+}
+
+class ExternalWidgetState extends State<ExternalWidget> {
+  bool _active = false;
+  void _handleBoxTap(bool active) {
+    setState(() {
+      _active = active;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: BorderBox(
+        active: _active,
+        onChanged: _handleBoxTap,
+      ),
+    );
+  }
+}
+
+class InternalWidgetState extends State<BorderBox> {
+  bool _highlight = false;
+
+  void _handleTapDown(TapDownDetails) {
+    setState(() {
+      _highlight = true;
+    });
+  }
+
+  void _handleTapUp(TapUpDetails) {
+    setState(() {
+      _highlight = false;
+    });
+  }
+
+  void _handleTapCancel() {
+    setState(() {
+      _highlight = false;
+    });
+  }
+
+  void _handleTap() {
+    widget.onChanged(!widget.active);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTapDown: _handleTapDown,
+      onTapUp: _handleTapUp,
+      onTapCancel: _handleTapCancel,
+      onTap: _handleTap,
+      child: Container(
+        child: Center(
+          child: Text(
+            widget.active ? 'Active' : 'Inactive',
+            style: TextStyle(
+              fontSize: 15,
+              color: Colors.white,
+            ),
+          ),
+        ),
+        width: 200.0,
+        height: 200.0,
+        decoration: BoxDecoration(
+          color: widget.active ? Colors.lightGreen[700] : Colors.grey[600],
+          border: _highlight
+              ? Border.all(
+                  color: Colors.orange,
+                  width: 10.0,
+                )
+              : null,
+        ),
+      ),
+    );
+  }
+}
+
+class BorderBox extends StatefulWidget {
+  BorderBox({Key? key, this.active = false, required this.onChanged})
+      : super(key: key);
+
+  final active;
+  final ValueChanged<bool> onChanged;
+
+  @override
+  InternalWidgetState createState() => InternalWidgetState();
 }
